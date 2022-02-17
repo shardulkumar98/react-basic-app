dd9ed21ffec5b43a6aa7de2cfb8e66b5822bf0c8
Shardul Kumar <shardulkumar@WIL-Admins-Mac-mini.local>
2022-02-17 18:09:07 +0530

    update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,35 +2,35 @@ import React from "react";
 import Navbar from "components/navbar";
 import Footer from "components/footer";
 import { Div, P, Form, Input, TextArea, Button } from "styles/pages/contact";
-const Contact = () => (
-  <>
-    <Navbar />
+import { useForm } from "react-hook-form";
 
-    <Div>
-      <P>Contact Us</P>
-      <Form>
-        <Input type="text" placeholder="Full Name" required />
-        <Input
-          type="text"
-          placeholder="Mobile Number"
-          maxLength="10"
-          required
-        />
-        <Input type="number" placeholder="Age" required />
-        <Input type="text" placeholder="Location" required />
-        <TextArea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          placeholder="Description"
-          maxLength="999"
-          required
-        ></TextArea>
-        <Button type="submit">Send</Button>
-      </Form>
-    </Div>
-    <Footer />
-  </>
-);
+const Contact = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <Navbar />
+      <Div>
+        <P>Contact Us</P>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Input type="text" {...register("name")} placeholder="Full Name" />
+          <Input type="text" {...register("mob")} placeholder="Mobile Number" />
+          <Input type="number" {...register("age")} placeholder="Age" />
+          <Input type="text" {...register("location")} placeholder="Location" />
+          <TextArea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            {...register("desc")}
+            placeholder="Description"
+          ></TextArea>
+          <Button type="submit">Send</Button>
+        </Form>
+      </Div>
+      <Footer />
+    </>
+  );
+};
 export default Contact;
