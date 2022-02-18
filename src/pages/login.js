ddf1d684ead3123bbff2ac851be0259447f4c9cf
Shardul Kumar <shardulkumar@WIL-Admins-Mac-mini.local>
2022-02-18 16:41:07 +0530

    final commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
-// import axios from "axios";
+import axios from "axios";
 
 import {
   Container,
