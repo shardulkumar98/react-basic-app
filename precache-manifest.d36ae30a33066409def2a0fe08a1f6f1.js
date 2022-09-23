ecb7e3ca74443c5efd0d921ebd88cee7d97ed406
Shardul Kumar <shardulkumar@WIL-Admins-Mac-mini-4.local>
2022-09-23 17:08:02 +0530

    Updates

---
+++ precache-manifest.d36ae30a33066409def2a0fe08a1f6f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abff2ff75512ab1b394153033e0a98d",
+    "url": "/react-basic-app/index.html"
+  },
+  {
+    "revision": "79d8c8c25452f6ee9b20",
+    "url": "/react-basic-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7da5bb70bae86520b448",
+    "url": "/react-basic-app/static/js/2.2a658d40.chunk.js"
+  },
+  {
+    "revision": "1b7cbb6df3972f750d01",
+    "url": "/react-basic-app/static/js/3.b0ad0487.chunk.js"
+  },
+  {
+    "revision": "79d8c8c25452f6ee9b20",
+    "url": "/react-basic-app/static/js/main.13b492d2.chunk.js"
+  },
+  {
+    "revision": "cd8eaee7eff6cfa82328",
+    "url": "/react-basic-app/static/js/runtime~main.338164cc.js"
+  },
+  {
+    "revision": "dd6fb6778429f855c5af97453a1887e1",
+    "url": "/react-basic-app/static/media/logo.dd6fb677.png"
+  }
+]);
